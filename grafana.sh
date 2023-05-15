bf3d441097b7bcbf3d8a3c2ca13ca2de58720df3
venkat1817 <dvenkataramir@gmail.com>
2023-05-15 11:06:46 +0530

    cicd-observability-userdata

--- grafana.sh
+++ grafana.sh
@@ -1,7 +1,19 @@
-#!/bin/bash
-cd /opt
+# #!/bin/bash
+# cd /opt
+# wget https://dl.grafana.com/oss/release/grafana-9.2.1.linux-amd64.tar.gz
+# tar -xvzf grafana-9.2.1.linux-amd64.tar.gz
+# chmod -R 755 grafana-9.2.1
+# cd grafana-9.2.1/bin/
+# nohup ./grafana-server & 
+
+ #!/bin/bash
+cd /tmp
 wget https://dl.grafana.com/oss/release/grafana-9.2.1.linux-amd64.tar.gz
 tar -xvzf grafana-9.2.1.linux-amd64.tar.gz
-chmod -R 755 grafana-9.2.1
-cd grafana-9.2.1/bin/
-nohup ./grafana-server & 
+mv grafana-9.2.1 grafana
+chmod -R 755 grafana
+cp -r * /opt
+cd /opt
+cd grafana
+cd bin
+nohup ./grafana-server &
